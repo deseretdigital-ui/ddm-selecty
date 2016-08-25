4221df577cc5ec2757654b3c45c7cd18c1d9df2c
AndriyHudyma <ahud@softserveinc.com>
2016-08-25 18:00:01 +0300

    Small changes to tests confiuration

--- testing/test-bundler.js
+++ testing/test-bundler.js
@@ -7,8 +7,5 @@ import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 chai.use(chaiEnzyme());
 
-// Include all .js files under `app`, except app.js, reducers.js, and routes.js.
-// This is for isparta code coverage
-const context = require.context('../src', true, /^^((?!(app|reducers|routes)).)*\.js$/);
-// const context = require.context('../src', true, /^^((test|tests).)*\.js$/);
+const context = require.context('./tests', true, /^.*\.js$/);
 context.keys().forEach(context);
---
+++ testing/tests/unit/first.test.js
@@ -0,0 +1,3 @@
+describe('First test', () => {
+  it('should be useful');
+});
