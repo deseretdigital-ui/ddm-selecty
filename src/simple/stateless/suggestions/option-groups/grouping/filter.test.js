e7df4aa1beb2b280c51b262365dc4ea7e2bdd752
Justin Macdonald <jmacdonald@deseretdigital.com>
2016-09-09 14:42:40 -0600

    Adds tests for the filter.js

---
+++ src/simple/stateless/suggestions/option-groups/grouping/filter.test.js
@@ -0,0 +1,50 @@
+import expect from 'expect';
+import { wordFilter } from './filter';
+
+describe('Grouping', () => {
+  describe('filter', () => {
+
+    // it('should properly sort options', () => {
+    //   // Arrange
+    //   const optLabel = 'label';
+    //   const options = [
+    //     { label: 'option_one', value: 1 },
+    //     { label: 'option_two', value: 2 },
+    //     { label: 'option_three', value: 3 },
+    //     { label: 'option_four', value: 4 },
+    //   ];
+
+    //   const expected = [
+    //     { label: 'option_four', value: 4 },
+    //     { label: 'option_one', value: 1 },
+    //     { label: 'option_three', value: 3 },
+    //     { label: 'option_two', value: 2 },
+    //   ];
+
+    //   // Act
+    //   const actual = sortOptions(options, optLabel, true);
+
+    //   // Assert
+    //   expect(actual).toEqual(expected);
+    // });
+
+    // it('should not sort if sortable flag is false', () => {
+    //   // Arrange
+    //   const optLabel = 'label';
+    //   const options = [
+    //     { label: 'option_one', value: 1 },
+    //     { label: 'option_two', value: 2 },
+    //     { label: 'option_three', value: 3 },
+    //     { label: 'option_four', value: 4 },
+    //   ];
+
+    //   // Act
+    //   const actual = sortOptions(options, optLabel, false);
+
+    //   // Assert
+    //   expect(actual).toEqual(options);
+    // });
+
+  });
+
+});
